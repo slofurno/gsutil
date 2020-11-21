8099bf0f51b31e80b636b0b16dcc838c3dd5a41d
Steve LoFurno <slofurno@gmail.com>
2020-11-21 21:33:24 +0000

    support list

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"crypto/tls"
 	"crypto/x509"
 	"encoding/base64"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"net"
@@ -17,6 +18,32 @@ import (
 
 	"cloud.google.com/go/storage"
 )
+
+func init() {
+	certs := x509.NewCertPool()
+	if ok := certs.AppendCertsFromPEM(getAlpineCerts()); !ok {
+		panic("failed to read certs")
+	}
+
+	tlsConfig := &tls.Config{
+		RootCAs: certs,
+	}
+
+	http.DefaultTransport = &http.Transport{
+		Proxy: http.ProxyFromEnvironment,
+		DialContext: (&net.Dialer{
+			Timeout:   30 * time.Second,
+			KeepAlive: 30 * time.Second,
+			DualStack: true,
+		}).DialContext,
+		ForceAttemptHTTP2:     true,
+		MaxIdleConns:          100,
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+		TLSClientConfig:       tlsConfig,
+	}
+}
 
 func getAlpineCerts() []byte {
 	decoded, err := base64.StdEncoding.DecodeString(AlpineCerts)
@@ -50,33 +77,49 @@ func parseGSPath(path string) (string, string) {
 	return parts[0], parts[1]
 }
 
-func doCopy(srcPath, dstPath string) {
+var units = []string{"B", "KiB", "MiB", "GiB", "TiB"}
+
+func humanSize(size int64) string {
+
+	for i := 0; i < len(units); i++ {
+		n := size >> 10
+		if n == 0 {
+			return fmt.Sprintf("%d %s", size, units[i])
+		}
+		size = n
+	}
+	return ""
+}
+
+func list(path string) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Minute*2)
 	defer cancel()
 
-	certs := x509.NewCertPool()
-	if ok := certs.AppendCertsFromPEM(getAlpineCerts()); !ok {
-		panic("failed to read certs")
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		panic(err)
 	}
 
-	tlsConfig := &tls.Config{
-		RootCAs: certs,
-	}
+	bucket, key := parseGSPath(path)
+	h := client.Bucket(bucket)
 
-	http.DefaultTransport = &http.Transport{
-		Proxy: http.ProxyFromEnvironment,
-		DialContext: (&net.Dialer{
-			Timeout:   30 * time.Second,
-			KeepAlive: 30 * time.Second,
-			DualStack: true,
-		}).DialContext,
-		ForceAttemptHTTP2:     true,
-		MaxIdleConns:          100,
-		IdleConnTimeout:       90 * time.Second,
-		TLSHandshakeTimeout:   10 * time.Second,
-		ExpectContinueTimeout: 1 * time.Second,
-		TLSClientConfig:       tlsConfig,
+	it := h.Objects(ctx, &storage.Query{
+		Prefix: key,
+	})
+
+	for {
+		oa, err := it.Next()
+		if err != nil {
+			return
+		}
+
+		fmt.Printf("%s %s %s\n", humanSize(oa.Size), oa.Updated.Format(time.RFC3339), oa.Name)
 	}
+}
+
+func doCopy(srcPath, dstPath string) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute*2)
+	defer cancel()
 
 	client, err := storage.NewClient(ctx)
 	if err != nil {
@@ -149,6 +192,7 @@ func main() {
 			panic("gsutil cp <src> <dst>")
 		}
 		doCopy(args[1], args[2])
-
+	case "ls":
+		list(args[1])
 	}
 }
